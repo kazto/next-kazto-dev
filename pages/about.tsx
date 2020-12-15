9161244b7b09046956bab830b464bd94d9647d90
Kazto TAKAHASHI <kazuto-takahashi@coo.ne.jp>
2020-12-15 18:58:36 +0900

    WIP

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,12 +2,37 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const AboutPage = () => (
-  <Layout title="About | Next.js + TypeScript Example">
-    <h1>About</h1>
-    <p>This is the about page</p>
+  <Layout title="About">
+    <p>I'm Kazto.</p>
+    <ul>
+      <li>ソフトウェア/バックエンド エンジニア</li>
+      <li>エンジニアリングマネージャ</li>
+      <li>エンジニア採用</li>
+    </ul>
+    <hr />
+    <h2>Vision</h2>
+    <div className="vmv">Engineering More Interesting.</div>
+    <div className="vmv">しごとを、もっと、おもしろく。</div>
+    <h2>Mission</h2>
+    <ul>
+      <li>ともに成長する</li>
+      <li>三方良しを実現する</li>
+      <li>つらみを排し、仕事に熱中できる環境を提供する</li>
+    </ul>
+    <h2>Value</h2>
+    <ul>
+      <li>いいオヤジたる</li>
+      <li>調和を提供する</li>
+      <li>革新を追求する</li>
+    </ul>
+    <hr />
+    <p>仕事とは、いつも困難が降りかかり、つらく、きびしいものです。</p>
+    <p>しかし、仕事が我が事であるかぎり、それは乗り越えるべき壁であると考えています。</p>
+    <p>一方で、つらさ、きびしさに打ち負けてしまう方も数多くいらっしゃいます。</p>
+    <p>少しでも、仕事が『おもしろい』ものであると感じることができる方を増やしたいという想いで勤めています。</p>
     <p>
       <Link href="/">
-        <a>Go home</a>
+        <a>return home</a>
       </Link>
     </p>
   </Layout>
