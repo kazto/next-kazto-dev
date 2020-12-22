deeddd166a8107dfcb25b32f48dd1d966eff6bd3
Kazto TAKAHASHI <kazuto-takahashi@coo.ne.jp>
2020-12-22 21:30:15 +0900

    WIP

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -8,3 +8,22 @@ export type User = {
   id: number
   name: string
 }
+
+export type CarricurumVitaeEntry = {
+  from_date: string
+  to_date: string 
+  period: string
+  title: string
+  role: string
+  skill: string
+  team: string
+  description: Array<string>
+}
+
+export type CarricurumVitaeData = {
+  speciality: string
+  skill: string
+  business: string
+  self_recommend: Array<string>
+  carriculum_vitae: Array<CarricurumVitaeEntry>
+}
