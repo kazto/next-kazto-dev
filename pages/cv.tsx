9df9a91a615c241519f94ba5a3213cb9d6f0db7d
Kazto TAKAHASHI <kazto@kazto.dev>
2021-01-11 19:41:50 +0900

    いったんリリース

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -1,75 +1,93 @@
-const CurriculumVitaePage = ({ data }:any) => {
-    let i;
-    const contents = [
-        {"ckey": "speciality", "value": "特徴"},
-        {"ckey": "skill", "value": "技術"},
-        {"ckey": "self_recommend", "value": "自己分析"},
-        {"ckey": "region_of_interest", "value": "興味分野"},
-    ]
-    let listBase = [];
+const CurriculumVitaePage = ({ data }: any) => {
+  let i;
+  const contents = [
+    { ckey: "speciality", value: "特徴" },
+    { ckey: "skill", value: "技術" },
+    { ckey: "self_recommend", value: "自己分析" },
+    { ckey: "region_of_interest", value: "興味分野" },
+  ];
+  const listBase = [];
 
-    for (i in contents){
-        listBase.push(
-        <div className="divtable" key={i.toString()}>
-            <div className="divtabletitle">{contents[i].value}</div>
-            <div className="divtablebody">{data[contents[i].ckey]}</div>
+  for (i in contents) {
+    listBase.push(
+      <div className="divtable" key={i.toString()}>
+        <div className="divtabletitle">{contents[i].value}</div>
+        <div className="divtablebody">{data[contents[i].ckey]}</div>
+      </div>
+    );
+  }
+
+  const listCV = [];
+  for (const n in data.carriculum_vitae) {
+    const i = data.carriculum_vitae.length - Number(n) - 1;
+    listCV.push(
+      <div className="divcvtable" key={i.toString()}>
+        <div className="divcvtablerow">
+          <div className="divtablecvtitle">自</div>
+          <div className="divtablecvbody">
+            {data.carriculum_vitae[i]["from_date"]}
+          </div>
+          <div className="divtablecvtitle">至</div>
+          <div className="divtablecvbody">
+            {data.carriculum_vitae[i]["to_date"]}
+          </div>
+          <div className="divtablecvtitle">期間</div>
+          <div className="divtablecvbody">
+            {data.carriculum_vitae[i]["period"]}
+          </div>
         </div>
-        )
-    }
-
-    let listCV = [];
-    for(let n in data.carriculum_vitae) {
-        let i = data.carriculum_vitae.length - Number(n) - 1
-        listCV.push(
-            <div className="divcvtable" key={i.toString()}>
-                <div className="divcvtablerow">
-                    <div className="divtablecvtitle">自</div>
-                    <div className="divtablecvbody">{data.carriculum_vitae[i]['from_date']}</div>
-                    <div className="divtablecvtitle">至</div>
-                    <div className="divtablecvbody">{data.carriculum_vitae[i]['to_date']}</div>
-                    <div className="divtablecvtitle">期間</div>
-                    <div className="divtablecvbody">{data.carriculum_vitae[i]['period']}</div>
-                </div>
-                <div className="divcvtablerow">
-                    <div className="divtablecvtitle">概要</div>
-                    <div className="divtablecvbody">{data.carriculum_vitae[i]['title']}</div>
-                    <div className="divtablecvtitle">役割</div>
-                    <div className="divtablecvbody">{data.carriculum_vitae[i]['role']}</div>
-                </div>
-                <div className="divcvtablerow">
-                    <div className="divtablecvtitle">技能</div>
-                    <div className="divtablecvbody">{data.carriculum_vitae[i]['skill']}</div>
-                    <div className="divtablecvtitle">チーム</div>
-                    <div className="divtablecvbody">{data.carriculum_vitae[i]['team']}</div>
-                </div>
-                <div className="divcvtablerow">
-                    <div className="divtablecvtitle">詳細</div>
-                    <div className="divtablecvbody">{data.carriculum_vitae[i]['description']}</div>
-                </div>
-            </div>
-        )
-    }
-
-    return (
-        <div>
-            <h1 className="cvtitle">職務経歴書</h1>
-            <div className="divbox">
-                {listBase}
-                <h2>職務経歴</h2>
-                {listCV}
-            </div>
+        <div className="divcvtablerow">
+          <div className="divtablecvtitle">概要</div>
+          <div className="divtablecvbody">
+            {data.carriculum_vitae[i]["title"]}
+          </div>
+          <div className="divtablecvtitle">役割</div>
+          <div className="divtablecvbody">
+            {data.carriculum_vitae[i]["role"]}
+          </div>
         </div>
-    )
-}
+        <div className="divcvtablerow">
+          <div className="divtablecvtitle">技能</div>
+          <div className="divtablecvbody">
+            {data.carriculum_vitae[i]["skill"]}
+          </div>
+          <div className="divtablecvtitle">チーム</div>
+          <div className="divtablecvbody">
+            {data.carriculum_vitae[i]["team"]}
+          </div>
+        </div>
+        <div className="divcvtablerow">
+          <div className="divtablecvtitle">詳細</div>
+          <div className="divtablecvbody">
+            {data.carriculum_vitae[i]["description"]}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="cvbox">
+      <h1 className="cvtitle">職務経歴書</h1>
+      <div className="divbox">
+        {listBase}
+        <h2>職務経歴</h2>
+        {listCV}
+      </div>
+    </div>
+  );
+};
 
 export async function getStaticProps() {
-    const res = await fetch('https://raw.githubusercontent.com/kazto/carriculum-vitae/master/carriculum-vitae.json')
-    const data = await res.json()
-    return {
-        props: {
-            data,
-        },
-    }
+  const res = await fetch(
+    "https://raw.githubusercontent.com/kazto/carriculum-vitae/master/carriculum-vitae.json"
+  );
+  const data = await res.json();
+  return {
+    props: {
+      data,
+    },
+  };
 }
 
-export default CurriculumVitaePage
+export default CurriculumVitaePage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,53 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import Layout from "../components/Layout";
 
 const IndexPage = () => (
   <Layout title="kazto.dev">
-    <div style={{position: "relative", width: '720px', margin: '50px auto'}}>
-      <img
-      src="/kazto_dev_logo.png"
-      alt="kazto.dev"
-      width={720}
-      height={323}
-      />
+    <div style={{ position: "relative", width: "720px", margin: "50px auto" }}>
+      <img src="/kazto_dev_logo.png" alt="kazto.dev" width={720} height={323} />
     </div>
-    <div style={{margin: '100px auto', padding: '100'}}>
+    <div style={{ margin: "100px auto", padding: "100" }}>
       <ul className="topul">
         <li>
-          <Link href="/about"><a>About</a></Link>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
         </li>
         <li>
-          <Link href="/cv"><a>Cariculum Vitae</a></Link>
+          <Link href="/cv">
+            <a>Cariculum Vitae</a>
+          </Link>
         </li>
-        <li><a href="https://twitter.com/bainarian">Twitter</a></li>
-        <li><a href="https://github.com/kazto">GitHub</a></li>
-        <li><a href="https://zenn.dev/kazto">Zenn</a></li>
-        <li><a href="https://wp.kazto.dev/">Blog</a></li>
-        <li><a href="https://www.wantedly.com/users/75211914">Wantedly</a></li>
-        <li><a href="https://www.linkedin.com/in/kazto/">LinkedIn</a></li>
-        <li><a href="https://teratail.com/users/kazto">Teratail</a></li>
-        <li><a href="https://speakerdeck.com/kazto">SpeakerDeck</a></li>
-        <li><a href="https://tamamoku.tokyo/">たまもく</a></li>
+        <li>
+          <a href="https://twitter.com/bainarian">Twitter</a>
+        </li>
+        <li>
+          <a href="https://github.com/kazto">GitHub</a>
+        </li>
+        <li>
+          <a href="https://zenn.dev/kazto">Zenn</a>
+        </li>
+        <li>
+          <a href="https://wp.kazto.dev/">Blog</a>
+        </li>
+        <li>
+          <a href="https://www.wantedly.com/users/75211914">Wantedly</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/kazto/">LinkedIn</a>
+        </li>
+        <li>
+          <a href="https://teratail.com/users/kazto">Teratail</a>
+        </li>
+        <li>
+          <a href="https://speakerdeck.com/kazto">SpeakerDeck</a>
+        </li>
+        <li>
+          <a href="https://tamamoku.tokyo/">たまもく</a>
+        </li>
       </ul>
     </div>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
