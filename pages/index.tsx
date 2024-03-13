b0c245f72ac00c7c35ef961af329b25e9a3d851a
Kazto TAKAHASHI <kazto@kazto.dev>
2024-03-13 23:20:43 +0900

    change link button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,61 +1,43 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
 
-const IndexPage = () => (
+const IndexPage = () => {
+  const links = [
+    {title: "About", url: "/about", image: "button-about.webp"},
+    {title: "Curriculum Vitae", url: "/cv", image: "button-cv.webp"},
+    {title: "Twitter", url: "https://twitter.com/kazto_dev", image: "button-xtwitter.webp"},
+    {title: "Bluesky", url: "https://bsky.app/profile/kazto.dev", image: "button-bluesky.webp"},
+    {title: "Lapras", url: "https://lapras.com/public/kazto_dev", image: "button-lapras.webp"},
+    {title: "YOUTRUST", url: "https://youtrust.jp/users/kazto", image: "button-youtrust.webp"},
+    {title: "GitHub", url: "https://github.com/kazto", image: "button-github.webp"},
+    {title: "Zenn", url: "https://zenn.dev/kazto", image: "button-zenn.webp"},
+    {title: "Blog(Under Maintainance)", url: "https://blog.kazto.dev", image: "button-blog.webp"},
+    {title: "Wantedly", url: "https://www.wantedly.com/users/75211914", image: "button-wantedly.webp"},
+    {title: "LinkedIn", url: "https://www.linkedin.com/in/kazto/", image: "button-linkedin.webp"},
+    {title: "Note.com", url: "https://note.com/kazto_dev", image: "button-note.webp"},
+    {title: "SpeakerDeck", url: "https://speakerdeck.com/kazto", image: "button-speakerdeck.webp"},
+    {title: "たまもく", url: "https://tamamoku.tokyo/", image: "button-tamamoku.webp"},
+  ]
+
+  return (
   <Layout title="kazto.dev">
     <div className="logobox">
       <img className="logo" src="/kazto_dev_logo.webp" alt="kazto.dev" />
     </div>
-    <div style={{ margin: "100px auto", padding: "100" }}>
-      <ul className="topul">
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/cv">Curriculum Vitae</Link>
-        </li>
-        <li>
-          <a href="https://twitter.com/kazto_dev">Twitter</a>
-        </li>
-        <li>
-          <a href="https://bsky.app/profile/kazto.dev">Bluesky</a>
-        </li>
-        <li>
-          <a href="https://lapras.com/public/kazto_dev">Lapras</a>
-        </li>
-        <li>
-          <a href="https://youtrust.jp/users/kazto">YOUTRUST</a>
-        </li>
-        <li>
-          <a href="https://github.com/kazto">GitHub</a>
-        </li>
-        <li>
-          <a href="https://zenn.dev/kazto">Zenn</a>
-        </li>
-        <li>
-          <a href="https://blog.kazto.dev/">Blog(Under maintenance)</a>
-        </li>
-        <li>
-          <a href="https://www.wantedly.com/users/75211914">Wantedly</a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/kazto/">LinkedIn</a>
-        </li>
-        <li>
-          <a href="https://note.com/kazto_dev">Note.com</a>
-        </li>
-        <li>
-          <a href="https://teratail.com/users/kazto">Teratail</a>
-        </li>
-        <li>
-          <a href="https://speakerdeck.com/kazto">SpeakerDeck</a>
-        </li>
-        <li>
-          <a href="https://tamamoku.tokyo/">たまもく</a>
-        </li>
+    <div className="linkdiv">
+      <ul className="linkul">
+        {links.map((link, i) => (
+          <li className="linkli" key={i}>
+            {
+              link.url.startsWith("https://") ?
+                (<a href={link.url}><img className="linkbutton" src={"images/" + link.image} alt={link.title} /></a>) :
+                (<Link href={link.url}><img className="linkbutton" src={"images/" + link.image} alt={link.title} /></Link>)
+            }
+          </li>
+        ))}
       </ul>
     </div>
   </Layout>
-);
+)};
 
 export default IndexPage;
